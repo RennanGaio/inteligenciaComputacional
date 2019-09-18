f2f5984b000554618d5a188d5f25dcc7c3213e89
Rennan Gaio <rennan.gaio12@gmail.com>
2019-09-18 11:39:14 -0300

    adding new dataset

--- trabalho2/src/dataset.py
+++ trabalho2/src/dataset.py
@@ -2,34 +2,36 @@ import pandas as pd
 from pprint import pprint
 from sklearn.model_selection import train_test_split
 
+dataset="banana"
+#dataset="phoneme"
 
 def split_data():
-    bananadata = pd.read_csv("../data/banana.csv")
-    bananadata_train, bananadata_test = train_test_split(bananadata, test_size=0.10)
+    dataset_data = pd.read_csv("../data/"+dataset+".csv")
+    data_train, data_test = train_test_split(dataset_data, test_size=0.10)
 
-    bananadata_train.to_csv("../data/bananadata_train.csv", sep='\t')
-    bananadata_test.to_csv("../data/bananadata_test.csv", sep='\t')
+    data_train.to_csv("../data/"+dataset+"data_train.csv", sep='\t')
+    data_test.to_csv("../data/"+dataset+"data_test.csv", sep='\t')
 
 
 if __name__ == '__main__':
     split_data()
 else:
-    bananadata_train = pd.read_csv("../data/bananadata_train.csv", sep='\t', index_col=0)
-    print(bananadata_train.shape)
-    X_train = bananadata_train.drop('Class', axis=1)
-    y_train = bananadata_train['Class']
+    data_train = pd.read_csv("../data/"+dataset+"data_train.csv", sep='\t', index_col=0)
+    print(data_train.shape)
+    X_train = data_train.drop('Class', axis=1)
+    y_train = data_train['Class']
 
-    bananadata_test = pd.read_csv("../data/bananadata_test.csv", sep='\t', index_col=0)
-    print(bananadata_test.shape)
-    X_test = bananadata_test.drop('Class', axis=1)
-    y_test = bananadata_test['Class']
+    data_test = pd.read_csv("../data/"+dataset+"data_test.csv", sep='\t', index_col=0)
+    print(data_test.shape)
+    X_test = data_test.drop('Class', axis=1)
+    y_test = data_test['Class']
 
-    bananadata = pd.read_csv("../data/banana.csv")
-    X = bananadata.drop('Class', axis=1)
-    y = bananadata['Class']
+    dataset_data = pd.read_csv("../data/"+dataset+".csv")
+    X = dataset_data.drop('Class', axis=1)
+    y = dataset_data['Class']
 
     data = dict(
-        bananadata=bananadata,
+        bananadata=dataset_data,
         X=X,
         y=y,
         X_train=X_train,
